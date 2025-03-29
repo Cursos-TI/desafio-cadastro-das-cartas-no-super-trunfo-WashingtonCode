ce6f9654294fd16553441f870d9184f9f2d3fe4f
WashingtonCode <202501483143@alunos.unifanor.edu.br>
2025-03-29 00:42:45 +0000

    Declaração de variáveis

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,16 +6,13 @@
 //Teste Washington Melo
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    //Declaração de variáveis
+
+    char estado[5], codigodacarta[10], cidade[50]; // Variáveis do tipo caractere
+    int populacao, pontosturisticos; // Variáveis do tipo inteiro
+    float area, pib; // Variáveis de ponto flutuante
+
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
     return 0;
 }
